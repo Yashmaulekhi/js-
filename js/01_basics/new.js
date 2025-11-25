e96d727a6900a65e19a97d3c9676dbe05d3e9f50
Yash Maulekhi <yashmaulekhi63@gmail.com>
2025-11-25 21:57:42 +0530

    done

---
+++ js/01_basics/new.js
@@ -0,0 +1,15 @@
+//see difference
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+1+2);
+console.log(1+2 +"2");
+let a="12";
+let b="12a";
+let x= Number(a);
+let y= Number(b);
+console.log(x);
+console.log(y);
+console.log(typeof(x));
+console.log(typeof(y));
+//boolean me empty string false or non emptty true convert ke baad
